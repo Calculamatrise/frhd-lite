b0b03dcaf053c062d140c3d37f43b4bf73b6645d
ObeyLordGoomy <Swaggymudkip69@gmail.com>
2020-03-05 16:05:16 -0500

    edit copyright details

--- extention/js/background.js
+++ extention/js/background.js
@@ -1,7 +1,7 @@
 /* 
 MIT License
 
-Copyright (c) 2019 ObeyLordGoomy
+Copyright (c) 2020 Calculus & ObeyLordGoomy
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
