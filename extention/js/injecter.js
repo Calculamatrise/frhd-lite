40fde58403761af629b83c89711081799824cf80
ObeyLordGoomy <Swaggymudkip69@gmail.com>
2020-03-08 19:53:01 -0400

    Update css
    Added line on selected tab of profile
    Made features have old banner

--- extention/js/injecter.js
+++ extention/js/injecter.js
@@ -5,3 +5,7 @@ window.BetaFRHD = {
 let inject = document.createElement('script');
 inject.setAttribute('src', chrome.runtime.getURL('js/main.js'));
 document.head.appendChild(inject);
+
+for (const element of document.getElementsByClassName('featured')) {
+    element.innerText = '';
+}
