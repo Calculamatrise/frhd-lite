fcdcc3641c5b0ad63e7e32e1dafd9871e74ec44d
Calculus <51772836+Calculus6@users.noreply.github.com>
2020-03-09 00:01:38 -0700

    Update background.js

--- extention/js/background.js
+++ extention/js/background.js
@@ -5,7 +5,8 @@ chrome.webRequest.onBeforeRequest.addListener(({ url }) => {
 }, {
     urls: [
 		'https://cdn.freeriderhd.com/free_rider_hd/assets/styles/combined/gui/combined.min.*.css',
-		'https://cdn.freeriderhd.com/free_rider_hd/styles/application/frhd_app.min.*.css'
+		'https://cdn.freeriderhd.com/free_rider_hd/styles/application/frhd_app.min.*.css',
+		'https://community.freeriderhd.com/css.php?css=xenforo,form,public&style*'
 	],
     types: ['stylesheet']
 }, ['blocking']);
