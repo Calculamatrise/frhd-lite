ed1af4aeb6716c24a02ab9d3a3d20103c972ebb2
ObeyLordGoomy <Swaggymudkip69@gmail.com>
2020-03-09 12:37:54 -0400

    remove main stuff from forums mod

--- extention/js/forums_injecter.js
+++ extention/js/forums_injecter.js
@@ -5,7 +5,3 @@ window.BetaForums = {
 let inject = document.createElement('script');
 inject.setAttribute('src', chrome.runtime.getURL('js/forums_main.js'));
 document.head.appendChild(inject);
-
-for (const element of document.getElementsByClassName('featured')) {
-    element.innerText = '';
-}
