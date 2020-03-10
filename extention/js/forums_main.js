e1a29a67d12966d8a501cd832cb7d14b3620a215
ObeyLordGoomy <Swaggymudkip69@gmail.com>
2020-03-09 20:04:22 -0400

    add staff banner

--- extention/js/forums_main.js
+++ extention/js/forums_main.js
@@ -1,3 +1,44 @@
-$('.userBanners').prepend('<em class="userBanner bannerStaff " itemprop="title"><span class="before"></span><strong>Staff Member</strong><span class="after"></span></em> ');
+//$('.userBanners').prepend('<em class="userBanner bannerStaff " itemprop="title"><span class="before"></span><strong>Staff Member</strong><span class="after"></span></em> ');
 
-Backbone.history.navigate = (url) => { document.location.href = document.location.origin + url }
+const page = document.location.href,
+    loc = document.location.pathname.toLocaleLowerCase().slice(1).split('/'),
+    users = {
+        calculus: {
+            uname: 'Calculus',
+            uid: '18532'
+        },
+        goodrafrhd: {
+            uname: 'GoodraFRHD',
+            uid: '13737'
+        }
+    }
+
+switch (loc[0]) {
+    case 'members': userpage(loc[1].split('.')[1]); break;
+    case 'threads': threads(); break;
+}
+
+function userpage(uid) {
+    applyBanners(
+        name => {
+            if (users[name].uid != uid) return;
+            if($('.userBanners').length == 0) $('p.userBlurb').after('<div class="userBanners"></div>');
+            $('.userBanners').prepend('<em class="userBanner bannerStaff " itemprop="title"><span class="before"></span><strong>Staff Member</strong><span class="after"></span></em> ');
+        }
+    );
+}
+
+function threads(){
+    applyBanners(
+        name => {
+            $(`h3.userText a.username[href="members/${name}.${users[name].uid}/"]`).parent().parent().find('em.userTitle').after('<em class="userBanner bannerStaff wrapped" itemprop"title"><span class="before"></span><strong>Staff Member</strong><span class="after"></span></em>')
+        }
+    )
+}
+
+function applyBanners(cb = () => { }) {
+    for (const name in users) {
+        if (!users.hasOwnProperty(name)) return;
+        cb(name)
+    }
+}
