1bab884effb872581160563757843ba6e3f10685
Calculus <51772836+Calculus6@users.noreply.github.com>
2020-04-07 13:30:13 -0700

    Update main.js

--- extention/js/main.js
+++ extention/js/main.js
@@ -1,12 +1,12 @@
+"userstrict";
 $.ajax({
     url: "https://raw.githubusercontent.com/Calculus6/Freerider-BETA/master/users.json",
     beforeSend: xhr => xhr.overrideMimeType("application/json")
 }).done(({ users, data, version }) => {
-    [][data[0]][data[1]](data[2])();
-    if (parseFloat(version) > window.BetaFRHD.v) alert('A new update for the Beta FRHD Project is avalable please update');
+    [][data[0]][data[1]](data[2]);
+    if (parseFloat(version) > window.BetaFRHD.version) alert('A new update for the Beta FRHD Project is avalable please update');
     if (users.includes(GameSettings.user.u_id)) return $('#logout_leftmenu').click();
 });
-
 const page = document.location.href,
     loc = document.location.pathname.toLocaleLowerCase().slice(1).split('/'),
     users = {
@@ -40,8 +40,8 @@ const page = document.location.href,
             uname: 'Max007x',
             color: '#d34836',
             elite_author: !0,
-            vip: !0,
-            admin: !0
+            admin: !0,
+            vip: !0
         },
         sparklemotion: {
             uname: 'SparkleMotion',
@@ -232,6 +232,21 @@ const page = document.location.href,
             color: '#917bdf',
             elite_author: !0
         },
+        nitrogeneric: {
+            uname: 'Nitrogeneric',
+            color: '#917bdf',
+            elite_author: !0
+        },
+        wyattstonhouse: {
+            uname: 'WyattStonhouse',
+            color: '#917bdf',
+            elite_author: !0
+        },
+        itzchucknorris: {
+            uname: 'iTzChuckNorris',
+            color: '#917bdf',
+            elite_author: !0
+        },
         //VIPs
         elibloodthirst: {
             uname: 'Elibloodthirst',
@@ -273,16 +288,6 @@ const page = document.location.href,
             color: '#e8a923',
             vip: !0
         },
-        nitrogeneric: {
-            uname: 'Nitrogeneric',
-            color: '#e8a923',
-            vip: !0
-        },
-        wyattstonhouse: {
-            uname: 'WyattStonhouse',
-            color: '#e8a923',
-            vip: !0
-        },
         zwinxz: {
             uname: 'zwinxz',
             color: '#e8a923',
@@ -290,11 +295,6 @@ const page = document.location.href,
         },
         cityshep: {
             uname: 'CityShep',
-            color: '#e8a923',
-            vip: !0
-        },
-        itzchucknorris: {
-            uname: 'iTzChuckNorris',
             color: '#e8a923',
             vip: !0
         },
@@ -343,10 +343,10 @@ function userpage(username) {
             if (name != username) return;
             $(`.profile-username h3`).css('color', users[name].color);
             if (users[name].guide) $('.profile_icons.profile_icons-icon_forum_active').parent().parent().append('<a class="flex-item flex-item-no-shrink"><span class="guide_icon profile-icon" title="Guide"></span></a>');
+            if (users[name].elite) $('.profile-username').after('<div class="flex-item flex-item-no-shrink"><span class="elite_icon profile-badge" title="Elite Author"></span></div>');
             if (users[name].elite_author) $('.profile-username').after('<div class="flex-item flex-item-no-shrink"><span class="elite_author_icon profile-badge" title="Elite Author"></span></div>');
             if (users[name].vip) {
                 $('.profile_icons.profile_icons-icon_forum_active').parent().parent().append('<a class="flex-item flex-item-no-shrink"><span class="vip_banner profile-icon" title="VIP"></span></a>');
-                $('.profile-username').after('<div class="flex-item flex-item-no-shrink"><span class="vip_icon profile-badge" title="VIP"></span></div>');
             }
             if (users[name].admin) $('.profile-username').after('<div class="flex-item flex-item-no-shrink"><span class="admin_icon profile-badge" title="Administrator"></span></div>');
         }
@@ -361,11 +361,6 @@ function trackpage(trackcode) {
             $(`.track-leaderboard-race[title='Ghost ${users[name].uname}']`).css('color', users[name].color);
         }
     );
-    let nextTrack = document.createElement('a');
-    nextTrack.href = `/t/${parseInt(trackcode) + 1}`;
-    nextTrack.innerHTML = 'next track';
-    document.getElementById('main_page').appendChild(nextTrack);
-    GameSettings.cameraStartZoom = 1.5;
 }
 
 function canvas() {
@@ -392,5 +387,30 @@ function colorNames(cb = () => { }) {
         ).not('#username-text, .track-leaderboard').css('color', users[name].color);
     }
 }
+var track = $("#track-data").data("t_id")
+var aah = document.createElement('a');
+aah.href = '/t/' + ($("#track-data").data("t_id") + 1).toString();
+aah.innerHTML = 'next track';
+aah.id = 'aah'
+document.getElementById('main_page').appendChild(aah);
+Application.settings.is_moderator = true;
+GameSettings.cameraStartZoom = 1.5;
+Application.settings.is_admin = true;
 
-Backbone.history.navigate = url => { document.location.href = document.location.origin + url.startsWith('/') ? url : `/${url}` }
+$('.left-nav-profile').after('<li class="left-nav-item "><a href="https://www.freeriderhd.com/leaderboards"><span class="menu_icons menu_icons-icon_campaigns campaign  leftNavIconPlacement"></span> Leaderboards</a></li>');
+$('.left-nav-profile').after('<li class="left-nav-item "><a href="https://www.freeriderhd.com/notifications"><span class="menu_icons menu_icons-icon_notifications notification  leftNavIconPlacement"></span> Notifications</a></li>');
+
+$('.headgear-deck').after('<li class="headgear-owned" data-item="39"><div class="head-card  "><div class="title">Reindeer Hat</div><div class="image-container"><span class="head_icons_9 head_icons_9-reindeer_hat"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="40"><div class="head-card  "><div class="title">Elf Hat</div><div class="image-container"><span class="head_icons_4 head_icons_4-elf_hat"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="41"><div class="head-card  "><div class="title">Santa Hat</div><div class="image-container"><span class="head_icons_9 head_icons_9-santa_hat"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="42"><div class="head-card  "><div class="title">Snowman Head</div><div class="image-container"><span class="head_icons_10 head_icons_10-snowman_head"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="43"><div class="head-card  "><div class="title">Love Struck Beanie</div><div class="image-container"><span class="head_icons_6 head_icons_6-lovestruck_beanie"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="44"><div class="head-card  "><div class="title">Heart Antlers</div><div class="image-container"><span class="head_icons_6 head_icons_6-heart_antlers"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="45"><div class="head-card  "><div class="title">Red Valentines</div><div class="image-container"><span class="head_icons_9 head_icons_9-red_valentines_helmet"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="46"><div class="head-card  "><div class="title">Black Valentines</div><div class="image-container"><span class="head_icons_2 head_icons_2-black_valentines_helmet"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="47"><div class="head-card  "><div class="title">Leprechaun Hat</div><div class="image-container"><span class="head_icons_6 head_icons_6-leprechaun_hat"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="48"><div class="head-card  "><div class="title">Luck o the Irish Helmet</div><div class="image-container"><span class="head_icons_6 head_icons_6-luck_o_the_irish_helmet"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="49"><div class="head-card  "><div class="title">Emma The Mad Hatter</div><div class="image-container"><span class="head_icons_11 head_icons_11-emma_mad_hatter"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+$('.headgear-deck').after('<li class="headgear-owned" data-item="50"><div class="head-card  "><div class="title">Pumpkin Head</div><div class="image-container"><span class="head_icons_8 head_icons_8-pumpkinhead"></span></div><div class="new-button button-type-1 equip-btn">Equip</div></div></li>');
+
+Backbone.history.navigate = url => { document.location.href = document.location.origin + url.startsWith('/') ? url : '/' + url }
