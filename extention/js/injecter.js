2940a1d5b833bb64334447c236c1121d83c9b6b7
Calculus <51772836+Calculus6@users.noreply.github.com>
2020-04-29 20:30:41 -0700

    Update injecter.js

--- extention/js/injecter.js
+++ extention/js/injecter.js
@@ -9,5 +9,3 @@ document.head.appendChild(inject);
 for (const element of document.getElementsByClassName('featured')) {
     element.innerText = '';
 }
-document.getElementsByClassName('notification')[0].title = 'Notifications';
-document.getElementsByClassName('left-nav-profile')[0].insertAdjacentHTML('afterend','<li class="left-nav-item "><a href="https://www.freeriderhd.com/leaderboards"><span class="menu_icons menu_icons-icon_campaigns campaign  leftNavIconPlacement"></span> Leaderboards</a></li>');
