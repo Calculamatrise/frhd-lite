8bcb4c6bb59708ca0205b952210fb92eaac57030
Calculus <51772836+Calculus0972@users.noreply.github.com>
2020-07-26 20:20:30 -0700

    Delete forums_injecter.js

--- extention/js/forums_injecter.js
+++
@@ -1,7 +0,0 @@
-window.mod = {
-    v: chrome.runtime.getManifest().version
-}
-
-let inject = document.createElement('script');
-inject.setAttribute('src', chrome.runtime.getURL('js/forums_main.js'));
-document.head.appendChild(inject);
