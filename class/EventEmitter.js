a6af8efb4eab56951a7bcb26e2120725c640f085
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-07-11 19:05:18 -0700

    Add files via upload

--- class/EventEmitter.js
+++ class/EventEmitter.js
@@ -1,16 +1,3 @@
-function s(t) {
-    return "function" == typeof t
-}
-function n(t) {
-    return "number" == typeof t
-}
-function r(t) {
-    return "object" == typeof t && null !== t
-}
-function o(t) {
-    return void 0 === t
-}
-
 export default class {
     constructor() {
         this._events = this._events || {};
@@ -20,24 +7,21 @@ export default class {
         this.defaultMaxListeners = 10;
     }
     setMaxListeners(t) {
-        if (!n(t) || 0 > t || isNaN(t))
+        if (typeof t != "number" || 0 > t || isNaN(t))
             throw TypeError("n must be a positive number");
         return this._maxListeners = t,
         this
     }
     emit(t) {
         var e, i, n, a, h, l;
-        if (this._events || (this._events = {}),
-        "error" === t && (!this._events.error || r(this._events.error) && !this._events.error.length)) {
-            if (e = arguments[1],
-            e instanceof Error)
+        if (this._events || (this._events = {}), t === "error" && (!this._events.error || typeof this._events.error == "object" && this._events.error !== null && !this._events.error.length)) {
+            if (e = arguments[1], e instanceof Error)
                 throw e;
             throw TypeError('Uncaught, unspecified "error" event.')
         }
-        if (i = this._events[t],
-        o(i))
+        if (i = this._events[t], i === void 0)
             return !1;
-        if (s(i))
+        if (typeof i == "function")
             switch (arguments.length) {
             case 1:
                 i.call(this);
@@ -55,7 +39,7 @@ export default class {
                     a[h - 1] = arguments[h];
                 i.apply(this, a)
             }
-        else if (r(i)) {
+        else if (typeof i == "object" && i !== null) {
             for (n = arguments.length,
             a = new Array(n - 1),
             h = 1; n > h; h++)
@@ -69,14 +53,14 @@ export default class {
     }
     addListener(t, e) {
         var n;
-        if (!s(e))
+        if (typeof e != "function")
             throw TypeError("listener must be a function");
         if (this._events || (this._events = {}),
-        this._events.newListener && this.emit("newListener", t, s(e.listener) ? e.listener : e),
-        this._events[t] ? r(this._events[t]) ? this._events[t].push(e) : this._events[t] = [this._events[t], e] : this._events[t] = e,
-        r(this._events[t]) && !this._events[t].warned) {
+        this._events.newListener && this.emit("newListener", t, typeof e.listener == "function" ? e.listener : e),
+        this._events[t] ? typeof this._events[t] == "object" && this._events[t] !== null ? this._events[t].push(e) : this._events[t] = [this._events[t], e] : this._events[t] = e,
+        this._events[t] == "object" && this._events[t] !== null && !this._events[t].warned) {
             var n;
-            n = o(this._maxListeners) ? i.defaultMaxListeners : this._maxListeners,
+            n = this._maxListeners === void 0 ? i.defaultMaxListeners : this._maxListeners,
             n && n > 0 && this._events[t].length > n && (this._events[t].warned = !0,
             console.error("(node) warning: possible EventEmitter memory leak detected. %d listeners added. Use emitter.setMaxListeners() to increase limit.", this._events[t].length),
             "function" == typeof console.trace && console.trace())
@@ -90,7 +74,7 @@ export default class {
             n || (n = !0,
             e.apply(this, arguments))
         }
-        if (!s(e))
+        if (typeof e != "function")
             throw TypeError("listener must be a function");
         var n = !1;
         return i.listener = e,
@@ -99,17 +83,17 @@ export default class {
     }
     removeListener(t, e) {
         var i, n, o, a;
-        if (!s(e))
+        if (typeof e != "function")
             throw TypeError("listener must be a function");
         if (!this._events || !this._events[t])
             return this;
         if (i = this._events[t],
         o = i.length,
         n = -1,
-        i === e || s(i.listener) && i.listener === e)
+        i === e || typeof i.listener == "function" && i.listener === e)
             delete this._events[t],
             this._events.removeListener && this.emit("removeListener", t, e);
-        else if (r(i)) {
+        else if (typeof i == "object" && i !== null) {
             for (a = o; a-- > 0; )
                 if (i[a] === e || i[a].listener && i[a].listener === e) {
                     n = a;
@@ -137,8 +121,7 @@ export default class {
             this._events = {},
             this
         }
-        if (i = this._events[t],
-        s(i))
+        if (i = this._events[t], typeof i == "function")
             this.removeListener(t, i);
         else
             for (; i.length; )
@@ -148,10 +131,10 @@ export default class {
     }
     listeners(t) {
         var e;
-        return e = this._events && this._events[t] ? s(this._events[t]) ? [this._events[t]] : this._events[t].slice() : []
+        return e = this._events && this._events[t] ? typeof this._events[t] == "function" ? [this._events[t]] : this._events[t].slice() : []
     }
     listenerCount(t, e) {
         var i;
-        return i = t._events && t._events[e] ? s(t._events[e]) ? 1 : t._events[e].length : 0
+        return i = t._events && t._events[e] ? typeof t._events[e] == "function" ? 1 : t._events[e].length : 0
     }
 }
