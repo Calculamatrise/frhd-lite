6e3ea128eaaafac55fdbbbef625210426ef6bb0d
Calculus <tahar.ayoob@gmail.com>
2022-03-29 01:59:34 -0700

    Delete class directory

--- class/utils/formatnumber.js
+++
@@ -1,9 +0,0 @@
-export default function(t) {
-    t = parseInt(t, 10);
-    let e = Math.floor(t / 6e4)
-        , i = (t - 6e4 * e) / 1e3;
-    return i = i.toFixed(2),
-    10 > e && (e = e),
-    10 > i && (i = "0" + i),
-    e + ":" + i;
-}
